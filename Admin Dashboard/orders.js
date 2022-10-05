312c4b6132fc5551ac6aedeafd06bea2b53de828
Harsh Bardolia <110019311+harsh-bardolia@users.noreply.github.com>
2022-10-05 12:08:51 +0530

    Add files via upload

---
+++ Admin Dashboard/orders.js
@@ -0,0 +1,38 @@
+const Orders= [
+    {
+        productName : 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName : 'Larvender KT141 Drone',
+        productNumber: '85122',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName : 'Ruko F11 Drone',
+        productNumber: '41256',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName : 'iPhone 11 Pro',
+        productNumber: '41141',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName : 'Apple iWatch',
+        productNumber: '89741',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName : 'One Plus 10 Pro',
+        productNumber: '33411',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    }
+]
